5181430bffe46c99822b7f5f98b9b038f81a672d
Abby Lortz <abilenelortz03@gmail.com>
2018-06-25 13:51:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(distance) {
+  let blocks = distance
+  blockNum = Math.abs(distance - 42)
+  return blockNum
+}
+
+function distanceFromHqInFeet(driving) {
+  return 
+}
+function distanceTravelledInFeet(walking) {
+  
+}
+
+function calculatesFarePrice(cost) {
+  if (cost > 400) {
+    return '0'
+  }
+}
